d51c7449b2b6c2604f7cb0b66e8a96fe9ecd8df9
hx355397 <hxu8@albany.edu>
2022-12-02 20:29:46 +0800

    integrate history part to the main view

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import Button from 'react-bootstrap/Button';
 import Change from "./component/Change";
 import Profile from "./component/Profile";
+import History from "./component/History";
 
 function App() {
   return (
     <div className="App">
         <Profile/>
-      <Change/>
+        <Change/>
+        <History/>
     </div>
   );
 }
